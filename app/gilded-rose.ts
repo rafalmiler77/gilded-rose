440c7d9e036a4086f88a7b9c545281d0aaa48b3e
rafalmiler77 <rafalmiler77@gmail.com>
2019-04-13 10:18:45 +0200

    Backstage extracted

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -18,44 +18,50 @@ export class GildedRose {
     }
 
     updateQuality() {
+        const changeQualityBy = (item: Item, quantity: number) => {
+            item.quality += quantity;
+        };
+
         for (let i = 0; i < this.items.length; i++) {
             const item = this.items[i];
 
             if (item.name == 'Sulfuras, Hand of Ragnaros') {
                 return this.items;
             }
+            if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+                if (item.quality < 50) {
+                    changeQualityBy(item, 1);
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                changeQualityBy(item, 1)
+                            }
+                        }
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                changeQualityBy(item, 1)
+                            }
+                        }
+                }
+                item.sellIn = item.sellIn - 1;
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                }
+                return this.items;
+            }
 
-            if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (item.name != 'Aged Brie') {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1
                 }
             } else {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1
-                    if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-                    }
                 }
             }
             item.sellIn = item.sellIn - 1;
             if (item.sellIn < 0) {
                 if (item.name != 'Aged Brie') {
-                    if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality
-                    }
+                    item.quality = item.quality - item.quality
                 } else {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1
